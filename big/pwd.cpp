2e3911165304599c6216a5f6e6b7379b98a6434e
HFerZHY <38683880+HFerZHY@users.noreply.github.com>
2021-12-21 21:06:02 +0800

    Update pwd.cpp
    处理越界问题

--- big/pwd.cpp
+++ big/pwd.cpp
@@ -2,9 +2,11 @@
 #include "global.h"
 
 void doPwd(int argc, char * argv[]){
-    if(strcmp(argv[1], "--help") == 0){
-        strcpy(gTerm.strout, "Print the name of the current working directory.\n");
-        return;
+    if(argc >= 2){
+        if(strcmp(argv[1], "--help") == 0){
+            strcpy(gTerm.strout, "Print the name of the current working directory.\n");
+            return;
+        }
     }
     strcpy(gTerm.strout, gTerm.wdir);
     strcat(gTerm.strout, "\n");
